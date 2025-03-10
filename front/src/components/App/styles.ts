49ff82fab551e9df55b19679ddeec2c2ad1905f9
kauarsantoss <kaua.santos@germinare.org.br>
2025-03-10 07:58:09 -0300

    Resolving separate folders

---
+++ front/src/components/App/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: auto auto;
+    background-color: #f0faff;
+    border-radius: 16px;
+    padding: 32px;
+    border: 1px solid #828c91;
+    height: 80vh;
+`
+
+export const Title = styled.h1`
+    font-family: cursive;
+`
