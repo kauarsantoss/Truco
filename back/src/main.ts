d3158d4b6dea747ba4a8faec03a73b2fc18892e0
Matheus Melo <matheus.melo@germinare.org.br>
2025-03-26 15:44:24 -0300

    adicionando cors para habilitar requisições externas

--- back/src/main.ts
+++ back/src/main.ts
@@ -4,10 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(TrucoModule);
+  app.enableCors({
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
     whitelist: true,
   }))
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
